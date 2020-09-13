9adddc255ea3b28fba530cb0c6342e60ee7dda5a
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-09-13 02:35:12 -0400

    Update pi_approximations.cpp

--- 2020-09-CppCon/SICP/pi_approximations.cpp
+++ 2020-09-CppCon/SICP/pi_approximations.cpp
@@ -1,13 +1,15 @@
 
-// https://www.godbolt.org/z/47KTjs
+// https://www.godbolt.org/z/joh4c3
 
 #include <vector>
 #include <numeric>   
 
 #include <range/v3/view/chunk.hpp>
+#include <range/v3/view/common.hpp>
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/transform.hpp>
-#include <range/v3/range/conversion.hpp>
+#include <range/v3/view/cycle.hpp>
+#include <range/v3/view/zip_with.hpp>
 
 #include <fmt/core.h>
 
@@ -17,27 +19,46 @@ namespace hs {
     template <typename T, typename BinOp>
     auto accumulate(T const& init, BinOp binop) {
         return ranges::make_pipeable([&](auto&& rng) {
+            auto t = rng | rv::common;
             return std::accumulate(
-                ranges::begin(rng), 
-                ranges::end(rng), 
+                ranges::begin(t), 
+                ranges::end(t), 
                 init, 
                 binop);
         });
     }
 }
 
-auto leibniz_pi_approximation(int n) -> double {
+auto leibniz_pi_approximation(int n) {
     return (rv::iota(0, n)
           | rv::transform([](auto e){ return 1 + 2 * e;})
           | rv::chunk(2)
           | rv::transform([](auto rng) { return 1.0 / (rng[0] * rng[1]); })
-          | ranges::to<std::vector<double>> // :( 
           | hs::accumulate(0.0, std::plus{})) * 8;
+}
+
+auto leibniz_pi_approximation2(int n) {
+    std::vector signs = {1,-1};
+    return (rv::zip_with(
+                [](auto e, auto sign) { return sign * 1.0 / e; }, 
+                rv::iota(0, n) | rv::transform([](auto e){ return 1 + 2 * e;}),
+                rv::cycle(signs))
+          | hs::accumulate(0.0, std::plus{})) * 4;
+}
+
+auto leibniz_pi_approximation2_alt(int n) {
+    return (rv::iota(0, n) 
+          | rv::transform([s = -1](auto e) mutable { 
+              s = s > 0 ? -1 : 1;
+              return s * 1.0 / (1 + 2 * e); })
+          | hs::accumulate(0.0, std::plus{})) * 4;
 }
 
 int main () {
 
     fmt::print("{}\n", leibniz_pi_approximation(10000));
+    fmt::print("{}\n", leibniz_pi_approximation2(10000));
+    fmt::print("{}\n", leibniz_pi_approximation2_alt(10000));
 
    return 0;
 }
