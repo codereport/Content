0b547e28d0b4272617a94d1aab52cac62272f869
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-09-13 21:57:57 -0400

    Update ramanujan.cpp

--- 2020-09-CppCon/SICP/ramanujan.cpp
+++ 2020-09-CppCon/SICP/ramanujan.cpp
@@ -16,14 +16,14 @@ namespace rv = ranges::views;
 namespace ra = ranges::actions;
 
 auto ramanujan_numbers() {
-    constexpr int upto = 33;
+    constexpr int N = 33;
 
     auto sum_cubes = [](auto const& t) -> int { 
         auto const [a, b] = t;
         return (a * a * a) + (b * b * b); 
     };
 
-    auto wpairs = rv::cartesian_product(rv::iota(1, upto), rv::iota(1, upto)) 
+    auto wpairs = rv::cartesian_product(rv::iota(1, N), rv::iota(1, N)) 
         | rv::filter([](auto r) { return std::get<0>(r) < std::get<1>(r); })
         | rv::transform(sum_cubes)
         | ranges::to<std::vector<int>>
