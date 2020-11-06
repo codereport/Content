8afcdbb21bb89e9677dbc4b56e7c18658f2f051f
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-11-06 12:57:24 -0500

    Create typeclasses.hs

---
+++ 2020-11-MeetingC++/ConceptsTraitsTypeClassesProtocols/typeclasses.hs
@@ -0,0 +1,26 @@
+
+class Shape a where
+    name      :: a -> String
+    area      :: a -> Float
+    perimeter :: a -> Float
+
+data Shapes = Circle    {r :: Float} |
+              Rectangle {w :: Float, h :: Float}
+
+instance Shape Shapes where
+    name      (Circle    _)   = "Circle"
+    area      (Circle    r)   = pi * r ^ 2
+    perimeter (Circle    r)   = 2 * pi * r
+    name      (Rectangle _ _) = "Rectangle"
+    area      (Rectangle w h) = w * h
+    perimeter (Rectangle w h) = 2 * w + 2 * h
+
+printShapeInfo :: Shape a => a -> IO()
+printShapeInfo s = putStrLn ("Shape: " ++ (name s)           ++ "\n" ++ 
+                             "Area:  " ++ show (area s)      ++ "\n" ++
+                             "Perim: " ++ show (perimeter s) ++ "\n")
+
+main :: IO ()
+main = do
+    printShapeInfo (Circle 1.0)
+    printShapeInfo (Rectangle 2.0 3.0)
