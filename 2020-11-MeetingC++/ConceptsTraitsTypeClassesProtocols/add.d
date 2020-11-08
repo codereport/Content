7297360a6455e08e521f30c36ae9abc4cfcb4f33
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2020-11-08 10:12:21 -0500

    Update add.d

--- 2020-11-MeetingC++/ConceptsTraitsTypeClassesProtocols/add.d
+++ 2020-11-MeetingC++/ConceptsTraitsTypeClassesProtocols/add.d
@@ -1,8 +1,9 @@
 
-// https://run.dlang.io/is/g6hXE1
+// https://run.dlang.io/is/XCZ1Uw
 
 import std.stdio;
 
+//int add(int a, int b) { return a + b; }
 T add(T)(T a, T b) { return a + b; }
 
 void main() {
