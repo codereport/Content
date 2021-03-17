3e780bf622faec70981b666ead88be708c4c056c
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2021-03-16 22:52:55 -0400

    Update elixir.exs

--- 2021-04-GTC/ThrustAndTheCppStandardAlgorithms/elixir.exs
+++ 2021-04-GTC/ThrustAndTheCppStandardAlgorithms/elixir.exs
@@ -2,6 +2,14 @@
 owners = ["Ahswin", "Lesley", "Sarah"]
 pets   = ["Dog",    "Cat",    "Dog"  ]
 
-dogOwners = Enum.zip   (owners, pets) 
-         |> Enum.filter(fn {_, pet} -> pet == "Dog" end)
-         |> Enum.map   (fn {owner, _} -> owner end)
+dogOwners = owners 
+    |> Enum.zip   (pets) 
+    |> Enum.filter(fn {_, pet} -> pet == "Dog" end)
+    |> Enum.map   (fn {owner, _} -> owner end)
+
+# Sum Evens
+vals = [42, 1729, 4104]
+
+sum_evens = vals 
+    |> Enum.filter(&Integer.is_even/1) 
+    |> Enum.sum
