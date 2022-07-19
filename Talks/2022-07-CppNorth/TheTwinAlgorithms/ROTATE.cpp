bf827d713d9dbe315d974372dd9732a56c63d61c
Conor Hoekstra <codereport@outlook.com>
2022-07-19 17:31:38 -0400

    Rotate

---
+++ Talks/2022-07-CppNorth/TheTwinAlgorithms/ROTATE.cpp
@@ -0,0 +1,25 @@
+
+// https://godbolt.org/z/sK55n89TM
+
+#include <algorithm>
+#include <string>
+
+#include <fmt/core.h>
+#include <fmt/ranges.h>
+
+using namespace std::string_literals;
+
+void rotate(auto f, auto m, auto l) {
+    std::reverse(f, m);
+    std::reverse(m, l);
+    std::reverse(f, l);
+}
+
+int main() {
+
+    auto conf = "C++North!"s;
+    rotate(conf.begin(), conf.begin() + 3, conf.end());
+    fmt::print("{}\n", conf);
+
+    return 0;
+}
