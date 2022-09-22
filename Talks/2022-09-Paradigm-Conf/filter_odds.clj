354ff0da397876769c792d9f44bf9eded8d13a30
Conor Hoekstra <codereport@outlook.com>
2022-09-22 19:33:57 -0400

    sum first two odds examples

--- Talks/2022-09-Paradigm-Conf/filter_odds.clj
+++ Talks/2022-09-Paradigm-Conf/filter_odds.clj
@@ -8,3 +8,17 @@
 
 (println (filter-odds list))
 (println (filter-odds1 list))
+
+;; Example of threading
+
+(defn sum-first-two-odds [list]
+    (reduce + (take 2 (filter odd? list))))
+
+(defn sum-first-two-odds1 [list]
+    (->> list
+         (filter odd?)
+         (take 2)
+         (reduce +)))
+
+(println (sum-first-two-odds list))
+(println (sum-first-two-odds1 list))
--- Talks/2022-09-Paradigm-Conf/filter_odds.hs
+++ Talks/2022-09-Paradigm-Conf/filter_odds.hs
@@ -3,9 +3,16 @@ filterOdds1    = filter odd
 filterOdds2    = filter (\e -> (mod e 2) == 1)
 filterOdds3 xs = [ x | x <- xs, odd x ]
 
+-- Translated clojure example
+
+sumFirstTwoOdds = sum
+                . take 2
+                . filter odd
+
 main :: IO ()
 main = do
     print $ filterOdds  [1..5]
     print $ filterOdds1 [1..5]
     print $ filterOdds2 [1..5]
     print $ filterOdds3 [1..5]
+    print $ sumFirstTwoOdds [1..5]
