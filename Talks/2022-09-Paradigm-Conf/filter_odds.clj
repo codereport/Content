8a003c842cbeed744bcb87dd4f7313229602da28
Conor Hoekstra <codereport@outlook.com>
2022-09-22 19:24:21 -0400

    Add Clojure example

--- Talks/2022-09-Paradigm-Conf/filter_odds.clj
+++ Talks/2022-09-Paradigm-Conf/filter_odds.clj
@@ -1,6 +1,10 @@
 (defn filter-odds [list]
     (filter odd? list))
 
+(defn filter-odds1 [list]
+    (filter #(= 1 (mod % 2)) list))
+
 (def list [1 2 3 4 5])
 
 (println (filter-odds list))
+(println (filter-odds1 list))
