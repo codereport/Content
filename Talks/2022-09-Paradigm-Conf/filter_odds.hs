a485706937af96dc67888c7e10764f4ba8f8af88
Conor Hoekstra <codereport@outlook.com>
2022-09-22 19:19:34 -0400

    Add another haskell example

--- Talks/2022-09-Paradigm-Conf/filter_odds.hs
+++ Talks/2022-09-Paradigm-Conf/filter_odds.hs
@@ -1,9 +1,11 @@
-filterOdds xs = filter odd xs
-filterOdds1   = filter odd
-filterOdds2   = filter (\e -> (mod e 2) == 1)
+filterOdds xs  = filter odd xs
+filterOdds1    = filter odd
+filterOdds2    = filter (\e -> (mod e 2) == 1)
+filterOdds3 xs = [ x | x <- xs, odd x ]
 
 main :: IO ()
 main = do
     print $ filterOdds  [1..5]
     print $ filterOdds1 [1..5]
     print $ filterOdds2 [1..5]
+    print $ filterOdds3 [1..5]
