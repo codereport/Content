85df033807a6b38a3d1f398da97c7eabccc51c11
Conor Hoekstra <codereport@outlook.com>
2024-07-23 14:57:44 -0400

    :sparkles: sort string examples

---
+++ Talks/2024-07-CppNorth/sort_string_1.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+#include <string_view>
+#include <vector>
+
+using namespace std::string_literals;
+
+namespace rv = std::ranges::views; // std::views
+
+auto sort_string(std::vector<int> nums, std::string_view chars) -> std::string {
+  auto pairs = std::vector<std::pair<int, char>>(nums.size());
+  auto str = std::string(nums.size(), ' ');
+  std::transform(nums.cbegin(), nums.cend(), chars.begin(), pairs.begin(),
+                 [](auto i, auto c) {
+                   return std::pair{i, c};
+                 });
+  std::sort(pairs.begin(), pairs.end());
+  std::transform(pairs.cbegin(), pairs.cend(), str.begin(),
+                 [](auto p) { return std::get<1>(p); });
+  return str;
+}
+
+auto main() -> int {
+
+  std::cout << sort_string(std::vector{3, 2, 1, 4}, "REPL"s) << '\n'; // "PERL"
+  std::cout << sort_string(std::vector{2, 4, 1, 3}, "AURK"s) << '\n'; // "RAKU"
+  std::cout << sort_string(std::vector{5, 4, 2, 6, 1, 3}, "OHYNPT"s)
+            << '\n'; // "RAKU"
+
+  return 0;
+}
---
+++ Talks/2024-07-CppNorth/sort_string_3.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <ranges>
+#include <string_view>
+#include <vector>
+
+#include <range/v3/range/conversion.hpp>
+
+using namespace std::string_literals;
+
+namespace rv = std::ranges::views; // std::views
+namespace v3 = ranges;
+
+auto sort_string(std::vector<int> nums, std::string_view chars) -> std::string {
+  auto pairs = rv::zip(nums, chars) | v3::to<std::vector>;
+  std::ranges::sort(pairs);
+  return rv::transform(pairs, [](auto p) { return std::get<1>(p); }) |
+         v3::to<std::string>;
+}
+
+auto main() -> int {
+
+  std::cout << sort_string(std::vector{3, 2, 1, 4}, "REPL"s) << '\n'; // "PERL"
+  std::cout << sort_string(std::vector{2, 4, 1, 3}, "AURK"s) << '\n'; // "RAKU"
+  std::cout << sort_string(std::vector{5, 4, 2, 6, 1, 3}, "OHYNPT"s)
+            << '\n'; // "RAKU"
+
+  return 0;
+}
